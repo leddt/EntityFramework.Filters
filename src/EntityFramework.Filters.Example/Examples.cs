af0229cadee103234390596bffd6e25469b92ee0
David Thibault <thibault.david@gmail.com>
2014-07-09 21:29:42 -0400

    Add failing tests relating to issue #8

--- src/EntityFramework.Filters.Example/Examples.cs
+++ src/EntityFramework.Filters.Example/Examples.cs
@@ -9,9 +9,9 @@
     {
         public Examples()
         {
-            //Database.SetInitializer(new DropCreateDatabaseAlways<ExampleContext>());
-            //SeedDb();
-            //Database.SetInitializer(new CreateDatabaseIfNotExists<ExampleContext>());
+            Database.SetInitializer(new DropCreateDatabaseAlways<ExampleContext>());
+            SeedDb();
+            Database.SetInitializer(new CreateDatabaseIfNotExists<ExampleContext>());
         }
 
         [Fact]
@@ -39,6 +39,67 @@
                     .ToList();
 
                 Assert.Equal(1, blogEntries.Count);
+            }
+        }
+
+        [Fact]
+        public void Should_support_disabling_filter()
+        {
+            using (var context = new ExampleContext())
+            {
+                var tenant = context.Tenants.Find(1);
+                context.CurrentTenant = tenant;
+                context.EnableFilter("Tenant")
+                    .SetParameter("tenantId", tenant.TenantId);
+
+                // Force query to execute a first time
+                context.BlogEntries.Count();
+
+                context.DisableFilter("Tenant");
+                Assert.Equal(2, context.BlogEntries.Count());
+            }
+        }
+
+        [Fact]
+        public void Should_support_changing_filter_parameter()
+        {
+            using (var context = new ExampleContext())
+            {
+                var tenant = context.Tenants.Find(1);
+                context.CurrentTenant = tenant;
+                context.EnableFilter("Tenant")
+                    .SetParameter("tenantId", tenant.TenantId);
+
+                var entry = context.BlogEntries.Find(1);
+                Assert.NotNull(entry);
+
+                tenant = context.Tenants.Find(2);
+                context.CurrentTenant = tenant;
+                context.EnableFilter("Tenant")
+                    .SetParameter("tenantId", tenant.TenantId);
+
+                entry = context.BlogEntries.Find(1);
+                Assert.Null(entry);
+            }
+        }
+
+        [Fact]
+        public void Should_not_persist_filter_accross_contexts()
+        {
+            using (var context = new ExampleContext())
+            {
+                var tenant = context.Tenants.Find(1);
+                context.CurrentTenant = tenant;
+                context.EnableFilter("Tenant")
+                    .SetParameter("tenantId", tenant.TenantId);
+
+                // Force query to execute a first time
+                context.BlogEntries.Count();
+            }
+
+            using (var context = new ExampleContext())
+            {
+                Assert.Equal(2, context.BlogEntries.Count());
             }
         }
 
